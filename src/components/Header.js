4881a39560645f18ac13c4d5d83acf2f07ca5826
sbyeol3 <ahnsbyeol3@gmail.com>
2020-05-29 17:01:12 +0900

    [#1] Style : Header CSS 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import './style.css'
-import Home from "./Home";
 
 const Header = () => {
   return(
-    <header>
-      <img/>
-      <nav>
-        <ul><li><NavLink to="/">Home</NavLink></li></ul>
-        <ul><li><NavLink to="/alumni">Alumni</NavLink></li></ul>
-        <ul><li><NavLink to="/portfolio">Portfolio</NavLink></li></ul>
-        <ul><li><NavLink to="/recruiting">Recruiting</NavLink></li></ul>
+    <header id="Header">
+      <nav className="nav-bar">
+        <div className="nav-bar-div">
+        <img className="header_logo" alt="logo"/>
+        <ul className="header_right">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/alumni">Alumni</NavLink></li>
+          <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+          <li><NavLink to="/recruiting">Recruiting</NavLink></li>
+        </ul>
+        </div>
       </nav>
     </header>
   )
